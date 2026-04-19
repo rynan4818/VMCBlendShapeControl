f7308b969624cafdf4ae0dddee5509b6e6dfacbe
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2026-04-19 20:33:40 +0900

    VersionUP

--- VMCBlendShapeControl/Properties/AssemblyInfo.cs
+++ VMCBlendShapeControl/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d9fb98b4-940e-4af8-b0ee-c9f24818ea69")]
-[assembly: AssemblyVersion("0.1.2")]
-[assembly: AssemblyFileVersion("0.1.2")]
+[assembly: AssemblyVersion("0.1.3")]
+[assembly: AssemblyFileVersion("0.1.3")]
